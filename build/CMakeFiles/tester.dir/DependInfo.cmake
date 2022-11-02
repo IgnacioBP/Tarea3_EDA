a785adde757f54fa7fe0515531bacabcdcd49544
IgnacioBP <iabravo1@miuandes.cl>
2022-11-01 23:59:54 -0300

    cosas

---
+++ build/CMakeFiles/tester.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/ibrav/Desktop/Tarea EDA/src/tree.cpp" "CMakeFiles/tester.dir/src/tree.cpp.obj" "gcc" "CMakeFiles/tester.dir/src/tree.cpp.obj.d"
+  "C:/Users/ibrav/Desktop/Tarea EDA/src/treeList.cpp" "CMakeFiles/tester.dir/src/treeList.cpp.obj" "gcc" "CMakeFiles/tester.dir/src/treeList.cpp.obj.d"
+  "C:/Users/ibrav/Desktop/Tarea EDA/src/treeListNode.cpp" "CMakeFiles/tester.dir/src/treeListNode.cpp.obj" "gcc" "CMakeFiles/tester.dir/src/treeListNode.cpp.obj.d"
+  "C:/Users/ibrav/Desktop/Tarea EDA/src/treeNode.cpp" "CMakeFiles/tester.dir/src/treeNode.cpp.obj" "gcc" "CMakeFiles/tester.dir/src/treeNode.cpp.obj.d"
+  "C:/Users/ibrav/Desktop/Tarea EDA/tests/tester.cpp" "CMakeFiles/tester.dir/tests/tester.cpp.obj" "gcc" "CMakeFiles/tester.dir/tests/tester.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
